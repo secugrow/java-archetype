482744c8f16492d688b4fb5c9635a1ac58614b33
exbwrubel <boris.wrubel@gmx.at>
2022-11-11 11:13:15 +0100

    Merge

--- src/main/resources/archetype-resources/src/test/java/stepdefinitions/AbstractStepDefs.java
+++ src/main/resources/archetype-resources/src/test/java/stepdefinitions/AbstractStepDefs.java
@@ -3,36 +3,108 @@
 #set( $symbol_escape = '\' )
 package ${package}.stepdefinitions;
 
+import com.deque.html.axecore.results.Rule;
+import io.cucumber.java8.Scenario;
 import ${package}.pageobjects.AbstractPage;
 import ${package}.pageobjects.MainPage;
 import ${package}.pageobjects.PageNotFoundException;
 import ${package}.utils.TestDataContainer;
+import ${package}.utils.TestDataContainer.Keys;
 import ${package}.webdriversession.WebDriverSession;
 import ${package}.webdriversession.WebDriverSessionStore;
+import ${package}.webdriversession.webdriverfactory.DriverType;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.io.FileUtils;
 import io.cucumber.java8.En;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+//a11y-start
+import ${package}.a11y.A11yHelper;
+//a11y-end
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.description.TextDescription;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.util.Collections;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public abstract class AbstractStepDefs implements En {
 
     public Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private TestDataContainer testDataContainer;
+    //a11y-start
+    private boolean skipA11Y = System.getProperty("skipA11y", "true").equalsIgnoreCase("true");
+    //a11y-end
 
     public AbstractStepDefs(TestDataContainer tdc) {
         logger.info("Constructor AbstractStepDefs");
         testDataContainer = tdc;
+        int debuglength = 80;
+        String fillchar = "#";
+
 
         Before(scenario -> {
-            logger.info("Before All Once");
-            testDataContainer.setTestDataString(TestDataContainer.Keys.BASEURL, System.getProperty(TestDataContainer.Keys.BASEURL.getKeyValue(), "baseUrl is not set, please add to your commandline '-DbaseUrl=yourvalue or add to your runConfigurtaion"));
+            logger.info("Before Scenario");
+            testDataContainer.setTestDataString(Keys.BASEURL, System.getProperty(Keys.BASEURL.getKeyValue(), "baseUrl is not set, please add to your commandline '-DbaseUrl=yourvalue or add to your runConfigurtaion"));
             String scenarioID = scenario.getName();
-            testDataContainer.setTestDataString(TestDataContainer.Keys.TEST_ID, scenarioID);
+            testDataContainer.setScenario(scenario);
+            testDataContainer.setTestDataString(Keys.TEST_ID, scenarioID);
+            testDataContainer.setTestDataBrowserType(DriverType.valueOf(System.getProperty("browser", "no browser set").toUpperCase()));
+            testDataContainer.setTestDataString("browser.version", System.getProperty("browser.version", "no version set"));
+            testDataContainer.setTestDataBoolean(Keys.INITIALIZED, false);
+            testDataContainer.setTestDataInt(Keys.STEP_INDEX, 0);
+            //a11y-start
+            testDataContainer.setTestDataBoolean(Keys.SKIP_A11Y, skipA11Y);
+            testDataContainer.setTestDataSoftAssertion("softAssertion.object", new SoftAssertions());
+            testDataContainer.setTestDataBoolean(Keys.SOFTASSERTIONS_ACTIVE, !skipA11Y);
+
+            if (!skipA11Y) {
+                testDataContainer.setTestDataList("a11y.description", new ArrayList<>());
+            }
+            //a11y-end
+
+            // to check if it runs on Jenkins or local
+            String jobname = System.getenv("JOB_NAME");
+
+            //Do Database resets here
+
+            if (jobname != null) {
+                testDataContainer.setTestDataBoolean(Keys.LOCALRUN, false);
+                logger.info(
+                        StringUtils.leftPad("JENKINS INFOS: ", debuglength, fillchar) + "\n" +
+                                String.format("BUILD_NUMBER: %s", System.getProperty("BUILD_NUMBER")) + "\n" +
+                                StringUtils.leftPad("# BUILD_NUMBER:" + System.getenv("BUILD_NUMBER"), debuglength, fillchar) + "\n" +
+                                StringUtils.leftPad("# JOB_NAME: " + System.getenv("JOB_NAME"), debuglength, fillchar) + "\n" +
+                                StringUtils.leftPad("# JENKINS_URL: " + System.getenv("JENKINS_URL"), debuglength, fillchar) + "\n" +
+                                StringUtils.leftPad("# WORKSPACE: " + System.getenv("WORKSPACE"), debuglength, fillchar) + "\n" +
+                                StringUtils.leftPad("# NODE_NAME: " + System.getenv("NODE_NAME"), debuglength, fillchar) + "\n" +
+                                StringUtils.leftPad(fillchar, debuglength, fillchar)
+                );
+
+            } else {
+                testDataContainer.setTestDataBoolean(Keys.LOCALRUN, true);
+            }
+
+
+
+            logger.debug(StringUtils.leftPad(fillchar, debuglength, fillchar));
+            logger.debug(StringUtils.leftPad("# executing Scenario: " +  scenario.getName(), debuglength, fillchar));
+            logger.debug(StringUtils.leftPad("# executing Scenario: " + scenario.getName(), debuglength, fillchar));
+            logger.debug(StringUtils.leftPad(fillchar, debuglength, fillchar));
+
+
+
+        });
+
+        BeforeStep(scenario -> {
+            testDataContainer.incrementStepIndex();
         });
 
         AfterStep(scenario -> {
@@ -44,13 +116,45 @@ public abstract class AbstractStepDefs implements En {
         After(scenario -> {
             logger.info("AfterAll Once");
 
+            String testId = testDataContainer.getTestId();
+
             if (scenario.isFailed()) {
                 TakesScreenshot takesScreenshot = ((TakesScreenshot) getCurrentWebDriver());
                 scenario.attach(takesScreenshot.getScreenshotAs(OutputType.BYTES), "image/png", "Screenshot");
                 scenario.attach(getCurrentWebDriver().getPageSource().getBytes(), "text/html", "Page Sourcecode");
+
+/*
+                if (isMobile) {
+                    //mobileScreenshot
+                }
+                else {
+                    //desktopscreenshot
+                }
+
+*/
+                //processing forced screenshots during test
+                testDataContainer.getScreenshots().forEach(screenshot -> {
+                    scenario.attach(screenshot.getLeft(), "image/png", "Forced Screenshot " + screenshot.getRight());
+
+                });
+
+                WebDriver webDriver = getCurrentWebDriverSession().getWebDriver();
+                if (testDataContainer.isLocalRun()) {
+                    File screenshot = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+                    FileUtils.copyFile(screenshot, new File(System.getProperty("user.dir") + "/target/error_selenium_" + testId + "_" + testDataContainer.getCurrentSessionId() + ".png"));
+                } else {
+                    byte[] screenshot = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES);
+                    scenario.attach(screenshot, "image/png", "Screenshot");
+                }
+
+
+
+
             }
             logger.info("Closing all open Sessions! That could be fatal for parallel Execution");
             WebDriverSessionStore.closeAll();
+
+
 
         });
     }
@@ -62,6 +166,7 @@ public abstract class AbstractStepDefs implements En {
     public WebDriverSession getCurrentWebDriverSession() {
         //TODO Add here thread name for junit 5 parallel execution
         String testID = testDataContainer.getTestId();
+        testDataContainer.setTestDataString("session.id", testID);
         return WebDriverSessionStore.getOrCreate(testID);
     }
 
@@ -79,6 +184,9 @@ public abstract class AbstractStepDefs implements En {
     public <T extends AbstractPage> T getPage(Class<T> pageClass) {
         Optional<T> page = getCurrentWebDriverSession().getPage(pageClass);
         if (page.isPresent()) {
+            //a11y-start
+            doA11YCheck();
+            //a11y-end
             return page.get();
         }
         throw new PageNotFoundException("page of type " + pageClass.getSimpleName() + " not open, but instead " + getCurrentPage());
@@ -91,5 +199,41 @@ public abstract class AbstractStepDefs implements En {
     protected String getTestIdForUser(String user) {
         return testDataContainer.getTestId().concat("_").concat(user);
     }
+
+
+    //a11y-start
+    private void doA11YCheck() {
+        if (testDataContainer.doA11YCheck()) {
+            Scenario scenario = testDataContainer.getScenario();
+            List<String> a11yExclusions = extractA11YExclusions(scenario);
+            WebDriver webDriver = getCurrentWebDriver();
+            List<Rule> issues = A11yHelper.hasAccessibilityIssues(webDriver, a11yExclusions);
+
+            if (!issues.isEmpty()) {
+                issues.forEach(violation -> {
+                    String violationString = "Violated Rule: " + violation.getId() + "on page " +
+                            getCurrentPage().toString() +
+                            violation.getNodes(); //fixme join nodes html
+
+                    testDataContainer.addScreenshot(((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES), "Forced A11y screenshot for step TODO " + violation.getDescription() );
+                    testDataContainer.addA11ydescription(violationString);
+                });
+
+
+
+                SoftAssertions softAssertions = testDataContainer.getSoftAssertionObject();
+                softAssertions.assertThat(issues).as(new TextDescription("Found ${issues.size} relevant A11Y violations in sceanrio '${scenario.name}' step# ${testDataContainer.getStepIndex()}"))
+                        .isEmpty();
+            }
+        }
+    }
+
+
+    private List<String> extractA11YExclusions(Scenario scenario) {
+        return scenario.getSourceTagNames().stream().filter(tagNames -> tagNames.startsWith("@A11YExclude:")).map(exclusion -> {
+            return exclusion.substring(exclusion.indexOf(":") + 1);
+        }).collect(Collectors.toList());
+    }
+//a11y-end
 
 }
--- src/main/resources/archetype-resources/src/test/java/utils/TestDataContainer.java
+++ src/main/resources/archetype-resources/src/test/java/utils/TestDataContainer.java
@@ -1,15 +1,36 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
+#set($symbol_pound='#')
+#set($symbol_dollar='$')
+#set($symbol_escape='\' )
 package ${package}.utils;
 
+import ${package}.webdriversession.webdriverfactory.DriverType;
+import io.cucumber.java8.Scenario;
+import org.apache.commons.lang3.tuple.Pair;
+import org.assertj.core.api.SoftAssertions;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.assertj.core.api.Fail.fail;
 
 public class TestDataContainer {
 
     public enum Keys {
         BASEURL("baseURL"),
-        TEST_ID("testId");
+        TEST_ID("testId"),
+        SCREENSHOTS("screenshots"),
+        LOCALRUN("localRun"),
+        //a11y-start
+        SKIP_A11Y("skipA11y"),
+        //a11y-end
+        INITIALIZED("initialized"),
+        STEP_INDEX("stepIndex"),
+        SOFTASSERTIONS_ACTIVE("softAssertions.present"),
+        SCENARIO("scenario");
 
         private final String key;
 
@@ -33,6 +54,22 @@ public class TestDataContainer {
         return getAs(Keys.TEST_ID.getKeyValue(), String.class);
     }
 
+    public void setScenario(Scenario scenario) {
+        testDataMap.put(Keys.SCENARIO.key, scenario);
+        testDataMap.put(Keys.TEST_ID.key, extractTestIdFromScenarioName(scenario));
+    }
+
+    //a11y-start
+    public boolean doA11YCheck() {
+        return !getAs(Keys.SKIP_A11Y.key, Boolean.class);
+    }
+    //a11y-end
+
+    public void incrementStepIndex() {
+        int stepIndex = getAs("stepIndex", Integer.class);
+        testDataMap.put("stepIndex", stepIndex++);
+    }
+
     public <T> T getAs(String key, Class<T> type) {
         if (testDataMap.containsKey(key)) {
             return type.cast(testDataMap.get(key));
@@ -49,6 +86,24 @@ public class TestDataContainer {
         return getAs(key, Integer.class);
     }
 
+    public String getCurrentSessionId() {
+        return getTestDataAsString("session.id");
+    }
+
+    public ArrayList<Pair<byte[], String>> getScreenshots() {
+
+        if (testDataMap.containsKey(Keys.SCREENSHOTS)) {
+            return getAs(Keys.SCREENSHOTS.key, ArrayList.class);
+
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    public boolean isLocalRun() {
+        return getAs(Keys.LOCALRUN.key, Boolean.class);
+    }
+
     public String getBaseUrl() {
         return getAs(Keys.BASEURL.getKeyValue(), String.class);
     }
@@ -58,6 +113,123 @@ public class TestDataContainer {
     }
 
     public void setTestDataString(Keys key, String stringValue) {
-        testDataMap.put(key.getKeyValue(), stringValue);
+        testDataMap.put(key.key, stringValue);
     }
+
+    public void setTestDataBoolean(Keys key, boolean boolenValue) {
+        testDataMap.put(key.getKeyValue(), boolenValue);
+    }
+
+    public void setTestDataInt(Keys key, int intValue) {
+        testDataMap.put(key.getKeyValue(), intValue);
+    }
+
+    public void setTestDataSoftAssertion(String key, SoftAssertions softAssertions) {
+        testDataMap.put(key, softAssertions);
+    }
+
+    public void setTestDataList(String key, List<String> list) {
+        testDataMap.put(key, list);
+    }
+
+    public void setTestDataBrowserType(DriverType driverType) {
+        testDataMap.put("browser.type", driverType);
+    }
+
+    private void setScreenshots(Keys key, List<Pair<byte[], String>> screenshots) {
+        testDataMap.put(Keys.SCREENSHOTS.key, screenshots);
+    }
+
+    public Scenario getScenario() {
+        return (Scenario) testDataMap.get("scenario");
+    }
+
+
+    public void addScreenshot(byte[] screenshot, String description) {
+        ArrayList<Pair<byte[], String>> screenshots = getScreenshots();
+        screenshots.add(Pair.of(screenshot, description));
+        setScreenshots(Keys.SCREENSHOTS, screenshots);
+    }
+
+
+    //a11y-start
+    public void addA11ydescription(String violationString) {
+        addStringtoList("a11y.description", violationString);
+    }
+
+    public SoftAssertions getSoftAssertionObject() {
+        return (SoftAssertions) testDataMap.get("softAssertion.object");
+    }
+
+    //a11y-end
+
+
+    private void addStringtoList(String key, String stringToAdd) {
+        if (testDataMap.containsKey(key)) {
+            ArrayList<String> list = getAs(key, ArrayList.class);
+            list.add(stringToAdd);
+            testDataMap.put(key, list);
+        }
+        else {
+            ArrayList<String> newList = new ArrayList<>();
+            newList.add(stringToAdd);
+            setTestDataList(key, newList);
+        }
+    }
+
+    private String extractTestIdFromScenarioName(Scenario scenario) {
+        Pattern regex = Pattern.compile("^\\[(.*?) ");
+        Matcher matcher = regex.matcher(scenario.getName());
+
+        try {
+            if (matcher.find()) {
+                return matcher.group(1);
+            }
+
+        } catch (NullPointerException npe) {
+            fail("Scenarioname is not correct formated $scenarioName. Pattern: '[XXX-99 [Filename]");
+        }
+        return "invalid Scenarioname Pattern";
+    }
+
+
+
+    //a11y-start
+    /*
+    Missing features from kotlin archetype
+
+
+
+
+    fun getScreenshots(): MutableList<Pair<ByteArray, String>> = getScreenshotArrayFromTestDataMap()
+
+    private fun getScreenshotArrayFromTestDataMap(): MutableList<Pair<ByteArray, String>> {
+        return when (testDataMap.containsKey(SCREENSHOTS)) {
+            true -> getAs(SCREENSHOTS)
+            false -> mutableListOf()
+        }
+    }
+
+
+
+    fun hasSoftAssertions() = getAs<Boolean>("softAssertions.present")
+
+
+
+    fun getAndClearA11Ydescriptions(): List<String> {
+        val descriptions = getA11yDescription()
+        testDataMap.replace("a11y.description", mutableListOf<String>())
+        return descriptions
+    }
+
+    fun getA11yDescription(): List<String> {
+        when (testDataMap.containsKey("a11y.description")) {
+            true -> return getAs("a11y.description")
+            false -> return emptyList()
+        }
+    }
+
+}
+     */
+    //a11y-end
 }
