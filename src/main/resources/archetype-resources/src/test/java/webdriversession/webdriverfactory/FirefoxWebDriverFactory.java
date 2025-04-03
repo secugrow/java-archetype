cc26fce246c9930a8d9c28d504b8d269280795af
Boris Wrubel <boris@secugrow.io>
2025-04-03 14:36:20 +0200

    Fixed FirefoxWebdriverFactory, Release Version 1.9.2

--- src/main/resources/archetype-resources/src/test/java/webdriversession/webdriverfactory/FirefoxWebDriverFactory.java
+++ src/main/resources/archetype-resources/src/test/java/webdriversession/webdriverfactory/FirefoxWebDriverFactory.java
@@ -3,18 +3,23 @@
 #set( $symbol_escape = '\' )
 package ${package}.webdriversession.webdriverfactory;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class FirefoxWebDriverFactory extends WebDriverFactory {
 
-    WebDriver webDriver;
+    private static final Logger LOGGER = LoggerFactory.getLogger(FirefoxWebDriverFactory.class);
 
     public WebDriver createDriver() {
-        WebDriverManager.firefoxdriver().driverVersion(super.getWebDriverVersion()).setup();
-        FirefoxOptions options = new FirefoxOptions();
-        return new FirefoxDriver(options.merge(caps));
+        FirefoxOptions ffOptions = new FirefoxOptions();
+        boolean linux = System.getProperty("os.name").toLowerCase().contains("linux");
+        if (linux && System.getenv().get("TMPDIR").contains("could_be_necessary_for_firefox_linux")) {
+            LOGGER.warn("If you running on Linux and using a snap installed firefox you have to set the TMPDIR to a writeable directory in your homedirectory with the absolute path!");
+        }
+        return new FirefoxDriver(ffOptions.merge(caps));
     }
 }
